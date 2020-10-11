303f2081ea4dd7e4f8afbe6852c860425f0b97a8
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-11 20:15:20 +0200

    Fixes.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const BinaryFile = require('binary-file');
+const fs = require('fs');
 
 const banner_patterns = require('./resources/banner_patterns.json');
 const block_id_map = require('./resources/block_id_map.json');
@@ -9,9 +9,9 @@ const item_id_map = require('./resources/item_id_map.json');
 const r12_block_states = require('./resources/r12_block_states.json');
 const recipes = require('./resources/recipes.json');
 
-const biome_definitions = new BinaryFile('./resources/biome_definitions.nbt', 'r');
-const entity_identifiers = new BinaryFile('./resources/entity_identifiers.nbt', 'r');
-const required_block_states = new BinaryFile('./resources/required_block_states.nbt', 'r');
+const biome_definitions = fs.readFileSync('./resources/biome_definitions.nbt');
+const entity_identifiers = fs.readFileSync('./resources/entity_identifiers.nbt');
+const required_block_states = fs.readFileSync('./resources/required_block_states.nbt');
 
 module.exports = {
     banner_patterns,
