3c98794398321b58ef824f2a1f50fe85edd92548
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-03-18 01:18:35 +0000

    chore(fix): Fix version.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const item_id_map = require('./resources/item_id_map.json');
 const recipes = require('./resources/recipes.json');
 
 const biome_definitions = fs.readFileSync(__dirname + '/resources/biome_definitions.nbt');
-const entity_identifiers = fs.readFileSync(__dirname + '/entity_identifiers.dat');
+const entity_identifiers = fs.readFileSync(__dirname + '/resources/entity_identifiers.dat');
 const canonical_block_states = fs.readFileSync(__dirname + '/resources/canonical_block_states.nbt');
 const r12_to_current_block_map = fs.readFileSync(__dirname + '/resources/r12_to_current_block_map.bin');
 const block_states = fs.readFileSync(__dirname + '/block_states.dat');
