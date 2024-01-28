b61aaf6b3cc1c46947f324b68c579cd431451cf1
Enrico Angelon <enricoangelon.ea@gmail.com>
2024-01-28 23:20:50 +0100

    fix states

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 const fs = require('fs');
 
 const banner_patterns = require('./resources/banner_patterns.json');
-const block_id_map = require('./resources/block_id_map.json');
+const block_id_map = require('./jsp/block_id_map.json');
 const creativeitems = require('./resources/creativeitems.json');
 const entity_id_map = require('./resources/entity_id_map.json');
-const item_id_map = require('./resources/item_id_map.json');
+const item_id_map = require('./jsp/item_id_map.json');
 const recipes = require('./resources/recipes.json');
 
 const biome_definitions = fs.readFileSync(__dirname + '/resources/biome_definitions.nbt');
 const entity_identifiers = fs.readFileSync(__dirname + '/resources/entity_identifiers.nbt');
-const canonical_block_states = fs.readFileSync(__dirname + '/canonical_block_states.nbt');
+const canonical_block_states = fs.readFileSync(__dirname + '/jsp/runtime_block_states.nbt');
 const r12_to_current_block_map = fs.readFileSync(__dirname + '/resources/r12_to_current_block_map.bin');
 
 module.exports = {
