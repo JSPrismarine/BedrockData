3fda4dd4f1abdf11918f83b9d7d84c2d4b0dded6
Filiph Siitam Sandström <filiph.sandstrom@filfatstudios.com>
2024-04-08 06:57:13 +0200

    Types: Remove left-over.

--- index.d.ts
+++ index.d.ts
@@ -7,11 +7,7 @@ declare module '@jsprismarine/bedrock-data' {
     export const recipes: any;
 
     export const biome_definitions: any;
-    export const biome_definitions_path: string;
     export const canonical_block_states: any;
-    export const canonical_block_states_path: string;
     export const entity_identifiers: any;
-    export const entity_identifiers_path: string;
     export const r12_to_current_block_map: any;
-    export const r12_to_current_block_map_path: string;
 }
